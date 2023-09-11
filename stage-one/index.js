07c37ab8c03c335b305d23b5f714f034742615c0
unsafemango <unsafemango@gmail.com>
2023-09-11 05:52:24 -0400

    finished stage one

--- stage-one/index.js
+++ stage-one/index.js
@@ -21,17 +21,21 @@ app.use(express.json());
 
 app.get("/api", (req, res) => {
   const { slack_name, track } = req.query;
+  const utc = new Date(
+    new Date().getTime() + new Date().getTimezoneOffset() * 60000
+  );
+  const utc_time = utc.toISOString().split(".")[0] + "Z";
 
-  res.json({
+  res.status(200).json({
     slack_name: slack_name,
-    currentDay: days[now.getDay()],
-    utc_time: now.toISOString(),
+    current_day: days[now.getDay()],
+    utc_time: utc_time,
     track: track,
     github_file_url:
       "https://github.com/unsafemango/hngx-backend/blob/main/stage-one/app.js",
     github_repo_url:
       "https://github.com/unsafemango/hngx-backend/tree/main/stage-one",
-    status_code: 200,
+    status_code: res.statusCode,
   });
 });
 
